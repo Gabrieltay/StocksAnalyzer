ddc3228dd4ac1422d57f20030a49732881bcc14a
gabriel_tay <gabriel_tay@tech.gov.sg>
2019-10-08 00:29:58 +0800

    Init

---
+++ src/main/kotlin/sg/fintech/stocks/analyzer/AnalyzerApplication.kt
@@ -0,0 +1,11 @@
+package sg.fintech.stocks.analyzer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnalyzerApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnalyzerApplication>(*args)
+}
